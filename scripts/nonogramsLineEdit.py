b8d07aa22c3a93ec2fc0f6ef0dd8a7a14f450c07
DobbyJjurado <juradojavier@hotmail.es>
2022-05-02 22:25:19 +0200

    Add Doctest and documentation to widget

--- scripts/nonogramsLineEdit.py
+++ scripts/nonogramsLineEdit.py
@@ -4,12 +4,41 @@ from PyQt5.QtCore import Qt
 from re import match
 from PyQt5.QtWidgets import QMessageBox
 
+"""
+Custom QLineEdit for Nonograms GUI
+"""
+
 
 class nonoGramsLineEdit(QLineEdit):
-	def __init__(self,parent, size: int, width:int = 50, height:int = 15):
+
+	"""
+	
+	Customizing QLineEdit for the Nonograms Resolver application. Inherits from QLineEdit
+
+	Attributes:
+		data (str): Data entered by the user.
+		width (int, optional): Widget width
+		height (int, optional): Widget heigth
+		size (int, private): NonoGrams Size
+
+	"""
+
+	def __init__(self,parent, size: int, width:int = 50, height:int = 15) -> None:
+
+		"""
+
+		The constructor for nonoGramsLineEdit class.
+  
+		Parameters:
+		   parent (QWidget): Parent on which I will display the widget
+		   size (int): NonoGrams Size
+		   width (int): Widget width
+		   height (int):  Widget heigth 
+		"""
+
 		QLineEdit.__init__(self, parent=parent)
-		self._size = size
-		self.data = ""
+		self.__size = size
+		self.data = []
 		self.width = width
 		self.height = height
 
@@ -17,29 +46,87 @@ class nonoGramsLineEdit(QLineEdit):
 		self.configure()
 
 
-	def connectSignals(self):
+	def connectSignals(self) -> None:
+	
+		"""
+
+		The function to connect widget signals.
+
+		"""
+
 		self.textChanged.connect(self.textSignal)
 
-	def configure(self):
+	def configure(self) -> None:
+		"""
+
+		The function to configure widget.
+		
+		"""
+
 		self.setAlignment(Qt.AlignCenter)
 		self.setFont(QFont("Arial", 7, QFont.ExtraBold))
 		self.setFixedWidth(self.width)
 		self.setFixedHeight(self.height)
 
-	def textSignal(self,text):
+	def textSignal(self, text: str) -> None:
+
+		"""
+
+		Function that makes sure that the text entered is only numbers and 
+		that the total of the numbers entered does not exceed the value of the size.
+
+		Parameters:
+			text (str): String to check.
+
+		>>> line_edit.textSignal("Hola Que tal")
+		>>> line_edit.data
+		[]
+
+		>>> line_edit.textSignal("4")
+		>>> line_edit.data
+		[4]
+
+		>>> line_edit.textSignal("2 2")
+		>>> line_edit.data
+		[2, 2]
+
+		>>> line_edit.textSignal("1 1 2")
+		>>> line_edit.data
+		[1, 1, 2]
+
+		>>> line_edit.textSignal("5 12")
+		>>> line_edit.data
+		[5]
+
+		>>> line_edit.textSignal("Juan")
+		>>> line_edit.data
+		[]
+		
+		
+		"""
 		if match("^[0-9 ]+$", text):
 			num_list = [int(i) for i in text.split(" ") if  i != ""]
-			if sum(num_list) <=  self._size:
+			if sum(num_list) <=  self.__size:
 				self.data = num_list
 			else:
-				QMessageBox.about(self, "Error", "The sum of the numbers entered is greater than the size of the nonogram.")
-				print(text[:-1])
+				if __name__ != "__main__": QMessageBox.about(self, "Error", "The sum of the numbers entered is greater than the size of the nonogram.")
 				self.setText(text[:-1])
+				self.data = num_list[:-1]
 		else:
 			self.clear()
+			self.data.clear()
 
 if __name__ == "__main__":
+	import doctest
 	import sys
+
+	
+	app = QApplication(sys.argv)
+	main = QWidget()
+	"Test with size 5"
+	doctest.testmod(extraglobs={'line_edit': nonoGramsLineEdit(main,size=5)})
+
+	"""
 	app = QApplication(sys.argv)
 	main = QWidget()
 	main.setMaximumSize(150, 150)
@@ -47,3 +134,4 @@ if __name__ == "__main__":
 	win = nonoGramsLineEdit(main, 2)
 	main.show()
 	sys.exit(app.exec_())
+	"""
--- scripts/nonogramsTableView.py
+++ scripts/nonogramsTableView.py
@@ -3,16 +3,43 @@ from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import * 
 from PyQt5 import QtCore
 import numpy as np
+
+"""
+Custom QTableWidget for Nonograms GUI
+"""
  
 class NonoGramsTable(QTableWidget):
-	def __init__(self, parent, size:int, data:list = []):
+
+	"""
+	
+	Customizing QTableWidget for the Nonograms Resolver application. Inherits from QTableWidget
+
+	Attributes:
+		size (int, private): Table size. size x size
+		data (list, optional): Data to represent in table.
+		minimun_size_table (int): Table size. By Default= 30
+
+	"""
+
+	def __init__(self, parent, size:int, data:list = []) -> None:
+
+		"""
+
+		The constructor for NonoGramsTable class.
+  
+		Parameters:
+			parent (QWidget): Parent on which I will display the widget
+		   	size (int): NonoGrams table size
+		   	data (list, optional): Data to represent in table.
+		"""
+
 		QTableWidget.__init__(self, parent=parent)
-		self._size = size
+		self.__size = size
 
 		if not data:
 			self.generateEmptyTable()
 		else:
-			self._data = data
+			self.__data = data
 
 		self.minimun_size_table = 30
 		self.setData()
@@ -21,6 +48,12 @@ class NonoGramsTable(QTableWidget):
 
 
 	def configureTableView(self) -> None:
+
+		"""
+
+		The function to configure widget.
+		
+		"""
 
 		# Configure vertical and horizontal Header
 		self.verticalHeader().setVisible(False)
@@ -44,16 +77,37 @@ class NonoGramsTable(QTableWidget):
 		# No focus
 		self.setFocusPolicy(QtCore.Qt.NoFocus)
 
+	def getData(self) -> list:
+		return self.__data
 
 	def generateEmptyTable(self) -> None:
-		self._data = np.eye(self._size, dtype= int)
+
+		"""
+		The function to generate matrix with size -> self.size.
+
+		>>> table.generateEmptyTable()
+		>>> table.getData()
+		array([[0, 0, 0],
+		       [0, 0, 0],
+		       [0, 0, 0]])
+
+		"""
+
+		self.__data = np.zeros((self.__size, self.__size),dtype= int)
  
 	def setData(self) -> None: 
-		self.setRowCount(self._size)
-		self.setColumnCount(self._size)
+		"""
 
-		for n, item in enumerate(self._data):
-			for m, item in enumerate(self._data[n]):
+		Function to generate the cells and paint them white if the element contains the value 0, 
+		otherwise they will be painted black.
+		
+		"""
+
+		self.setRowCount(self.__size)
+		self.setColumnCount(self.__size)
+
+		for n, item in enumerate(self.__data):
+			for m, item in enumerate(self.__data[n]):
 				newitem = QTableWidgetItem(item)
 				if item == 0:
 					newitem.setBackground(QColor(255,255,255))
@@ -64,23 +118,60 @@ class NonoGramsTable(QTableWidget):
 
 	
 	def configureWidthHeights(self) -> None:
-		for i in range(self._size):
+		"""
+
+		Function to configure the size of the cells. This size is indicated by the minimun_size_table parameter.
+
+		"""
+		for i in range(self.__size):
 			self.setColumnWidth(i, self.minimun_size_table)
 			self.setRowHeight(i,self.minimun_size_table)
 		
-		self.setGeometry(self.minimun_size_table*self._size, self.minimun_size_table*self._size, self.minimun_size_table*self._size, self.minimun_size_table*self._size)
+		self.setGeometry(self.minimun_size_table*self.__size, self.minimun_size_table*self.__size, self.minimun_size_table*self.__size, self.minimun_size_table*self.__size)
 
 
 	def update(self, size: int) -> None:
+		"""
+		
+		Function to generate a new table. 
+
+		Parameters:
+			size (int): New Size.
+
+		>>> table.update(4)
+		>>> table.getData()
+		array([[0, 0, 0, 0],
+		       [0, 0, 0, 0],
+		       [0, 0, 0, 0],
+		       [0, 0, 0, 0]])
+
+		>>> table.update(3)
+		>>> table.getData()
+		array([[0, 0, 0],
+		       [0, 0, 0],
+		       [0, 0, 0]])
+
+		"""
 		self.clear()
 		self.setRowCount(0)
 		self.setColumnCount(0)
 
-		self._size = size
+		self.__size = size
 		self.generateEmptyTable()
 		self.setData()
 
 if __name__ == "__main__":
+
+	import doctest
+	import sys
+
+	
+	app = QApplication(sys.argv)
+	main = QWidget()
+	"Test with size 3"
+	doctest.testmod(extraglobs={'table': NonoGramsTable(main,size=3)})
+
+	"""
 	import sys
 	app = QApplication(sys.argv)
 	main = QWidget()
@@ -90,3 +181,4 @@ if __name__ == "__main__":
 	win.update(4)
 	main.show()
 	sys.exit(app.exec_())
+	"""
